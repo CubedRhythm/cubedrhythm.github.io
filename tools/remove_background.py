a3ee39993b601bfaa7efb66419788886ae8c4723
CubedRhythm <80733322+CubedRhythm@users.noreply.github.com>
2025-12-23 14:58:43 -0800

    Polish: Archived Boss Rex and Generated Menacing Velociraptors (v7)

--- tools/remove_background.py
+++ tools/remove_background.py
@@ -7,11 +7,15 @@ def remove_white_background(image_path):
         img = Image.open(image_path)
         img = img.convert("RGBA")
         datas = img.getdata()
+        
+        # Get background color from top-left pixel
+        bg_r, bg_g, bg_b, bg_a = datas[0]
 
         newData = []
         for item in datas:
-            # Change all white (also shades of whites) to transparent
-            if item[0] > 240 and item[1] > 240 and item[2] > 240:
+            # Change pixels matching background color (within tolerance) to transparent
+            # Tolerance of 30
+            if abs(item[0] - bg_r) < 30 and abs(item[1] - bg_g) < 30 and abs(item[2] - bg_b) < 30:
                 newData.append((255, 255, 255, 0))
             else:
                 newData.append(item)
